59b1fa44688b466c6de517762fc64d4f94a79c47
Sai Varun <saivarunsvrn@gmail.com>
2019-08-10 11:49:30 +0530

    Add files via upload

--- ftlabs/mysite/searching/urls.py
+++ ftlabs/mysite/searching/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path('<str:s>/', views.index, name='index'),]
+urlpatterns = [path('', views.index, name='index'),path('ans/<str:s>/',views.ans,name='ans'),path('ans/',views.ans1,name='ans1')]
--- ftlabs/mysite/searching/views.py
+++ ftlabs/mysite/searching/views.py
@@ -3,17 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import Engwords
+from django.template import loader
 
-def index(request,s):
-    results_list=Engwords.objects.filter(word__contains=s)
-    list_startwith={}
-    list_notstartwith={}
-    for w in results_list:
-        if(w.word.startswith(s)):
-            list_startwith[w.word]=w.count
-        else:
-            list_notstartwith[w.word]=w.count
-    l=sorted(list_startwith.items(), key = lambda x : len(x[0]))
-    l1=sorted(list_notstartwith.items(), key = lambda x : len(x[0]))
-    l.extend(l1)
-    return HttpResponse(l[0:25])
+def index(request):
+    template = loader.get_template('searching/index.html')
+    context={'body':'testing',}
+    return HttpResponse(template.render(context,request))
+
+def ans1(request):
+    return HttpResponse("Type Something...")
+
+def ans(request,s):
+    if(len(s)==0):
+        return HttpResponse(" ")
+    else:
+        results_list=Engwords.objects.filter(word__contains=s)
+        list_startwith={}
+        list_notstartwith={}
+        for w in results_list:
+            if(w.word.startswith(s)):
+                list_startwith[w.word]=w.count
+            else:
+                list_notstartwith[w.word]=w.count
+        result=sorted(list_startwith.items(), key = lambda x : len(x[0]))
+        l1=sorted(list_notstartwith.items(), key = lambda x : len(x[0]))
+        result.extend(l1)
+        result=result[0:25]
+        return HttpResponse(result)
+        
